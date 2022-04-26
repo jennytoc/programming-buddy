48fb0d5965da3643322c43d2d805f56840d92504
jennytoc <toctocanj@gmail.com>
2022-04-26 10:35:25 -0600

    added env

--- backend/programbuddy_proj/settings.py
+++ backend/programbuddy_proj/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8jh#2@i0+lk3u_@nw4z91*=-3ct7-!+wz(hx*#t7#1c(8e0r5'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
